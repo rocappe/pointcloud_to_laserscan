47c7ac7f93349202ee89b72609bd5d802a663fbf
rocappe <38754046+rocappe@users.noreply.github.com>
2021-11-09 11:25:44 +0100

    Update cut_pointcloud_node.cpp

--- src/cut_pointcloud_node.cpp
+++ src/cut_pointcloud_node.cpp
@@ -1,43 +1,3 @@
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2010-2012, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- *
- */
-
-/*
- * Author: Paul Bovbel
- */
-
 #include "pointcloud_to_laserscan/cut_pointcloud_node.hpp"
 
 #include <chrono>
@@ -234,11 +194,6 @@ void CutPointCloudNode::cloudCallback(
         angle, angle_min_, angle_max_);
       continue;
     }
-    //uint8_t transparancy = 0;
-    //auto rgba = std::make_shared<uint32_t>(((*iter_r)<<24) + ((*iter_g)<<16) + ((*iter_b)<<8) + transparancy);
-    // uint8_t r = 255, g = 0, b = 0;    // Example: Red color
-    //*uint32_t rgb = ((std::uint32_t)r << 16 | (std::uint32_t)g << 8 | (std::uint32_t)b);
-    //*p.rgb = *reinterpret_cast<float*>(&rgb);
     auto point = std::make_shared<pcl::PointXYZRGB>(*iter_r, *iter_g, *iter_b);
     point->x = float (*iter_x);
     point->y = float (*iter_y);
